ab0132492a5265353ee08c225b307589cc162a6b
fulltilt1977 <fulltiltapps01@gmail.com>
2026-04-09 23:44:16 +1000

    Add initial RestoViz application files with structure and placeholders.

---
+++ styles/commonStyles.js
@@ -0,0 +1,10 @@
+// commonStyles.js
+import { StyleSheet } from 'react-native';
+
+export const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
